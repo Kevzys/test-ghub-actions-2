6602b0ccc495781a03612f643843180e7926fc97
Kevin <kevinamehta@hotmail.com>
2020-12-07 23:57:06 -0500

    pulling file from ark using requests

--- run.py
+++ run.py
@@ -1,1 +1,14 @@
-print('Hello World')
+import requests
+
+ARKG_LINK = 'https://www.ark-funds.com/auto/trades/ARK_ARKG_Trades.xls'
+#ARKK_LINK = 'https://www.ark-funds.com/auto/trades/ARK_ARKK_Trades.xls'
+#ARKQ_LINK = 'https://www.ark-funds.com/auto/trades/ARK_ARKQ_Trades.xls'
+#ARKW_LINK = 'https://www.ark-funds.com/auto/trades/ARK_ARKW_Trades.xls'
+
+print('Downloading excel files please wait...')
+
+f = requests.get(ARKG_LINK)
+
+output = open('test.xls', 'wb')
+output.write(f.content)
+output.close()
